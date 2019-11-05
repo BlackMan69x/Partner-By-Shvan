e0241dbc94ff2d220cd610b27001dff813956b6f
Glitch (hello-sqlite) <none>
2019-11-05 02:06:41 +0000

    ✍️🐂 Checkpoint ./server.js:867163/1092

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 // where your node app starts
 
 // init project
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
-var fs = require("fs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const fs = require("fs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -16,14 +16,13 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // init sqlite db
-var fs = require("fs");
-var dbFile = "./.data/sqlite.db";
-var exists = fs.existsSync(dbFile);
-var sqlite3 = require("sqlite3").verbose();
-var db = new sqlite3.Database(dbFile);
+const dbFile = "./.data/sqlite.db";
+const exists = fs.existsSync(dbFile);
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(dbFile);
 
 // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
-db.serialize(function() {
+db.serialize(() => {
   if (!exists) {
     db.run("CREATE TABLE Dreams (dream TEXT)");
     console.log("New table Dreams created!");
@@ -36,31 +35,29 @@ db.serialize(function() {
     });
   } else {
     console.log('Database "Dreams" ready to go!');
-    db.each("SELECT * from Dreams", function(err, row) {
+    db.each("SELECT * from Dreams", (err, row) => {
       if (row) {
-        console.log("record:", row);
+        console.log(`record: ${row}`);
       }
     });
   }
 });
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+app.get("/", (request, response) => {
+  response.sendFile(`${__dirname}/views/index.html`);
 });
 
 // endpoint to get all the dreams in the database
-// currently this is the only endpoint, ie. adding dreams won't update the database
-// read the sqlite3 module docs and try to add your own! https://www.npmjs.com/package/sqlite3
-app.get("/getDreams", function(request, response) {
-  db.all("SELECT * from Dreams", function(err, rows) {
+app.get("/getDreams", (request, response) => {
+  db.all("SELECT * from Dreams", (err, rows) => {
     response.send(JSON.stringify(rows));
   });
 });
 
-// Add a dream
-app.post("/addDream", function(request, response) {
-  console.log("add to dreams", request.body);
+// endpoint to add a dream to the database
+app.post("/addDream", (request, response)  => {
+  console.log(`add to dreams ${request.body}`);
 
   // DISALLOW_WRITE is an ENV variable set to TRUE in this template (in .env)
   // When you remix the app, DISALLOW_WRITE will be reset, andyou can add to the database
@@ -78,12 +75,17 @@ app.post("/addDream", function(request, response) {
   }
 });
 
+// endpoint to clear dreams from the database
+app.get('/clearDreams', (request, response) => {
+   db.each("SELECT * from Dreams", (err, row) => {
+    db.run(`DELETE FROM Dreams WHERE ID=?`, rows.id)
+  });
+});
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+var listener = app.listen(process.env.PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`);
 });
 
 // TODO
 // - add "clear" to delete all items (CRUDDY FOR REALS)
-// - remove comment that says "this won't write to db"
-// - const, let, arrow functions, backticks
