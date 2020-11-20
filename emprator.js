cd50a0aa583e1268b9c550ffc9e438f21658080e
Glitch (radical-uneven-dandelion) <none>
2020-11-20 22:01:54 +0000

    🌞🍿 Checkpoint ./emprator.js:24948434/3511

--- emprator.js
+++ emprator.js
@@ -22,12 +22,10 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const data = new Map();
 
-client.login(process);
-
 client.on("ready", () => {
   console.log(
     `Online In Servers : ${client.guilds.size} | Users : ${client.users.size}`
-  ); 
+  );
   let statuses = [
     `Servers: ${client.guilds.size} | Users: ${client.users.size}`,
     `PARTNER BOT `,
@@ -44,7 +42,7 @@ client.on("ready", () => {
 ////////////
 client.on("ready", () => {
   var join = client.channels.get("776849362642272277"); /// ئاید ڤۆیس
-  if (join) join.join();          
+  if (join) join.join();
 });
 
 client.on("message", saewan => {
@@ -53,13 +51,11 @@ client.on("message", saewan => {
       saewan.author.send(`>
 https://discord.gg/ZFyjJ7aAdy`); /// لینکە سیرڤەرە خۆت
 
-      client.channels.get("776849362642272277").send(
-                            ///ئایدی تێکست
+      client.channels.get("760103301575344148").send(
+        ///ئایدی تێکست
         `> Nerdra la layan <@${saewan.author.id}> 
 ${saewan.content}`
       );
     }
   }
 });
-
-client.login("");
